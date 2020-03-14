9d1c5f7a05894bb426a64123043f5900f90a8030
Youngho Cho <dominoyh5@gmail.com>
2020-03-14 14:03:54 +0900

    Create TempEraser Line and Coner

---
+++ Assets/Script/Stage/TempEraserController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempEraserController : MonoBehaviour
+{
+    Rigidbody eraser_rb;
+    public float torque;
+    
+    void Start()
+    {
+        eraser_rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        eraser_rb.AddTorque(new Vector3(torque, 0, 0));
+    }
+}
